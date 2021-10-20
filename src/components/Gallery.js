2118a13474d37e514e2339c6bc98a545efe0b977
Sebastian Rosado <s.rosado@uniandes.edu.co>
2021-10-19 22:18:18 -0500

    Modificaciones

---
+++ src/components/Gallery.js
@@ -0,0 +1,16 @@
+async function getresults(heightRef) {
+    const resp = await fetch('api?input=${heightRef}');
+    const data = await resp.json();
+    console.log('data from back', data);
+    //printValues(data);
+  }
+  var id,nombre,nombre,status, imagen;
+fetch(
+    "https://rickandmortyapi.com/api/character"
+  ).then((response) => response.json()).then((data) => {
+    id = data[0].results;
+    nombre= data[1].results;   
+    satus = data[2].results; 
+    gender = data[5].results; 
+    imagen = data[0].image;
+  });
